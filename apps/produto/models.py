a68e588f3fa7143ac10ac20dde05b120eedc2d6a
w <Gabriel Vilela>
2022-01-09 15:54:46 -0300

    Criando model t_produto

--- apps/produto/models.py
+++ apps/produto/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from PIL import Image
+import os
 
-# Create your models here.
+
+class T_produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao_curta = models.TextField(max_length=255)
+    descricao_longa = models.TextField()
+    imagem = models.ImageField(upload_to="produto_imagens/%Y%m/", blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    preco_marketing = models.FloatField()
+    preco_marketing_promocional = models.FloatField(default=0)
+    tipo = models.CharField(
+        default="V", max_length=1, choices=(("v", "Variação"), ("s", "Simples"))
+    )
